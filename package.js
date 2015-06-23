e9276efdee33f38ae0aa0b000d6beed74f419c95
Mohamed Ibrahim <mo3dev@gmail.com>
2015-06-23 11:18:03 -0400

    Updated to version 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mo3dev:image-resizer',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'A small library that provides capabilities to resize large images on the client-side',
   git: 'https://github.com/mo3dev/image-resizer.git',
   documentation: 'README.md'
